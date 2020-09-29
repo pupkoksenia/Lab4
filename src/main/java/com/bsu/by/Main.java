bdef317cd449cccfd9a8f7c4898a0bd9463ee4dc
Kseniya <pupkoksenia@gmail.com>
2020-09-29 21:51:14 +0300

    the forth step;have a nice day!)

--- src/main/java/com/bsu/by/Main.java
+++ src/main/java/com/bsu/by/Main.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,7 +25,11 @@ public class Main {
                 System.out.println("File already exists.");
             }
 
-            readAndMakeListOfLines("InputText.txt");
+            List<String> firstStep = readAndMakeListOfLines("InputText.txt");
+            List<Company> secondStep = makeClassesCompany(firstStep);
+            for (Company com : secondStep) {
+                System.out.println(com.Name);
+            }
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -33,6 +39,7 @@ public class Main {
     }
 
     private static List<String> readAndMakeListOfLines(String fileName) throws IOException {
+
         File file = new File(fileName);
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
@@ -47,14 +54,69 @@ public class Main {
         return ArrayOfLines;
     }
 
-    private static List<Company> makeClassesCompany(List<String> ArrayOfLines){
-        List<Company> Companies = new ArrayList<Company>();
+    public static boolean isDigit(String s) {
+        String regex = "[0-9]*\\.?[0-9]*";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(s);
+        boolean b = m.matches();
+        return b;
     }
-    class Company {
+
+
+    private static List<Company> makeClassesCompany(List<String> ArrayOfLines) {
+        List<Company> Companies = new ArrayList<Company>();
+
+        for (String line : ArrayOfLines) {
+            int beg = 0;
+            List<String> first = new ArrayList<>();
+
+            StringBuilder str = new StringBuilder(line);
+
+            for (int j = 0; j < line.length(); j++) {
+
+                if (line.charAt(j) == ';') {
+                    char[] dst = new char[j - beg];
+                    str.getChars(beg, j, dst, 0);
+                    first.add(String.valueOf(dst));
+                    beg = j + 1;
+
+                }
+            }
+            char[] dst = new char[line.length() - beg];
+            str.getChars(beg, line.length(), dst, 0);
+            first.add(String.valueOf(dst));
+            //first.add(dst.toString());
+
+            List<String> second = new ArrayList<>();
+            List<Integer> third = new ArrayList<>();
+
+            for (String f : first) {
+                if (isDigit(f)) {
+                    third.add(Integer.valueOf(f));
+                } else second.add(f);
+            }
+
+
+            Company a = new Company();
+            a.addIntegers(third.get(0), third.get(1), third.get(2), third.get(3));
+            a.addStrings(second.get(0), second.get(1), second.get(2), second.get(3),
+                    second.get(4), second.get(5), second.get(6), second.get(7));
+
+            Companies.add(a);
+        }
+
+
+        return Companies;
+
+
+    }
+
+
+    static class Company {
         String Name;
         String ShortName;
         Integer ActualDate;
-        Integer Address;
+        String Address;
         Integer FoundDate;
         Integer AmountOfEmploye;
         String Auditor;
@@ -64,19 +126,24 @@ public class Main {
         String KindOfActivity;
         String AddressInTheInternet;
 
-        Company(String Name, String ShortName, Integer ActualDate, Integer Address, Integer FoundDate, Integer AmountOfEmploye, String Auditor, Integer PhoneNumber, String Email, String Branch, String KindOfActivity, String AddressInTheInternet) {
+        void addStrings(String Name, String ShortName, String Address, String Auditor, String Email,
+                        String Branch, String KindOfActivity, String AddressInTheInternet) {
             this.Name = Name;
             this.ShortName = ShortName;
-            this.ActualDate = ActualDate;
             this.Address = Address;
-            this.FoundDate = FoundDate;
-            this.AmountOfEmploye = AmountOfEmploye;
             this.Auditor = Auditor;
-            this.PhoneNumber = PhoneNumber;
             this.Email = Email;
             this.Branch = Branch;
             this.KindOfActivity = KindOfActivity;
             this.AddressInTheInternet = AddressInTheInternet;
+        }
+
+        void addIntegers(Integer ActualDate,
+                         Integer FoundDate, Integer AmountOfEmploye, Integer PhoneNumber) {
+            this.ActualDate = ActualDate;
+            this.FoundDate = FoundDate;
+            this.AmountOfEmploye = AmountOfEmploye;
+            this.PhoneNumber = PhoneNumber;
         }
 
     }
